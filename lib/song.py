a9b4777b908946545a76e629cb891c5efcacaf53
Rizz-ke <wafiqmohameddoo@gmail.com>
2024-06-10 13:55:25 +0300

    Assignment completed

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
+# song.py
 class Song:
-    pass
+    count = 0
+    genre_count = {}
+    artist_count = {}
+    genres = set()
+    artists = set()
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+       
+        # Update genres
+        if genre not in Song.genre_count:
+            Song.genre_count[genre] = 0
+        Song.genre_count[genre] += 1
+        Song.genres.add(genre)
+       
+        # Update artists
+        if artist not in Song.artist_count:
+            Song.artist_count[artist] = 0
+        Song.artist_count[artist] += 1
+        Song.artists.add(artist)
+
